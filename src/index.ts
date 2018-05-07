fcf887dece4283468736f295f21335451f58bb9f
Robus Gauli <robusgauli@gmail.com>
2018-05-07 19:47:11 +0545

    Docs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+/**
+ * Error codes
+ */
 const NULL_OR_UNDEFINED = 100;
 const NAN = 200;
 const OUT_OF_RANGE = 300;
@@ -10,12 +13,26 @@ const UNDEFINED_OR_NULL_ATTRIBUTES = 500;
  *
  * @returns {object}
  */
-// tslint:disable-next-line:typedef
 function FunctionType(validationObject: { readonly [key: string]: any }) {
   return validationObject;
 }
 
-function NumberType(validationObject: any) {
+/**
+ * Signature function for defining number type .
+ * 
+ * # Example (es imports)
+ * import Types from 'type';
+ * const validator = {
+ *  length: Types.NumberType({
+ *    range: [1, 100]
+ *  })
+ * }
+ * 
+ * @param {object} validationObject 
+ * 
+ * @returns {function} 
+ */
+function NumberType(validationObject: any = {}) {
   return function(value: any) {
     return new class {
       public validate() {
@@ -44,70 +61,93 @@ function NumberType(validationObject: any) {
         );
 
         if (generatedErrors.length) {
-          // there is a error
           const payload = generatedErrors.reduce((acc: any, key: any) => {
             return errorPayload[key][1] === OUT_OF_RANGE
               ? { ...acc, [OUT_OF_RANGE]: validationObject.range }
               : { ...acc, [errorPayload[key][1]]: null };
           }, {});
+
           return {
-            error: true,
-            payload
-          };
-        } else {
-          return {
-            error: false
+            payload,
+            error: true
           };
         }
+
+        return {
+          error: false
+        };
+        
       }
     }();
   };
 }
 
+/**
+ * Signature function for defining String type .
+ * 
+ * # Example (es imports)
+ * import Types from 'type';
+ * const validator = {
+ *  color: Types.StringType({
+ *    oneOf: ['RED', 'GREEN']
+ *  })
+ * }
+ * 
+ * @param {object} validationObject 
+ * 
+ * @returns {function} 
+ */
 function StringType(validationObject: any) {
+  
   return function(value: any) {
     return new class {
       public validate() {
         const isInRange = validationObject.minLength
           ? value.length >= validationObject.minLength
           : true;
-
         const isOneOf =
           validationObject.oneOf && Array.isArray(validationObject.oneOf)
             ? validationObject.oneOf.indexOf(value) !== -1
             : true;
 
-        const errorPayload: { readonly [key: string]: boolean } = {
+        const payload: { readonly [key: string]: boolean } = {
           isInRange,
           isOneOf
         };
         if (
-          Object.keys(errorPayload).filter(key => errorPayload[key] === false)
+          Object.keys(payload).filter(key => payload[key] === false)
             .length >= 1
         ) {
-          // there is a error
-          const payload = {
-            error: true,
-            errorPayload
+          const errorPayload = {
+            payload,
+            error: true
           };
           if (!isOneOf) {
             return {
-              ...payload,
+              ...errorPayload,
               validStringTypes: validationObject.oneOf
             };
           }
+
           return payload;
-        } else {
-          return {
-            error: false,
-            errorPayload
-          };
         }
+
+        return {
+          payload,
+          error: false
+        };
+      
       }
     }();
   };
 }
 
+/**
+ * Functions that throws and error if argument passed to the constructor is mismatched from the intented argument.
+ * 
+ * @param {object} constructorArgument 
+ * @param {object} validationObject 
+ */
 function validateObjectConstruction(
   constructorArgument: any,
   validationObject: any
@@ -138,7 +178,7 @@ function validateObjectConstruction(
   }
   const args = Object.keys(constructorArgument);
   const errorPayload: { [key: string]: {} } = {};
-  let error: boolean = false;
+  let error = false;
   args.forEach(arg => {
     if (validationObject[arg]) {
       const result = validationObject[arg](constructorArgument[arg]).validate();
@@ -148,12 +188,23 @@ function validateObjectConstruction(
       }
     }
   });
+
   return {
     error,
     errorPayload
   };
 }
 
+/**
+ * Wrapper function that wraps around the method of the class 
+ * to enforce the type.
+ * 
+ * @param {object} validationObject 
+ * @param {Array} requiredArgumentList 
+ * @param {Array} requiredAttributes 
+ * 
+ * @returns {Function}
+ */
 function requiredFunctionParameters(
   validationObject: any,
   requiredArgumentList: ReadonlyArray<any>,
@@ -163,54 +214,65 @@ function requiredFunctionParameters(
     return function(argumentObject: any) {
       const target = this;
       if (
-        !(typeof argumentObject === 'object') ||
-        `${argumentObject}` === 'null'
+        requiredArgumentList &&
+        Array.isArray(requiredArgumentList)
       ) {
-        throw new Error(
-          'Must pass a valid object to the constructor function.'
+        if (
+          !(typeof argumentObject === 'object') ||
+          `${argumentObject}` === 'null'
+        ) {
+          throw new Error(
+            'Must pass a valid object to the function.'
+          );
+        }
+        const parametersDiff = requiredArgumentList.filter(
+          arg => Object.keys(argumentObject).indexOf(arg) === -1
         );
+        if (parametersDiff.length >= 1) {
+
+          return {
+            error: true,
+            errorPayload: {
+              code: MISMATCH_PARAMETERS,
+              data: parametersDiff,
+              message: 'Parameters missing.'
+            }
+          };
+        }
       }
-      const parametersDiff = requiredArgumentList.filter(
-        arg => Object.keys(argumentObject).indexOf(arg) === -1
-      );
-      if (parametersDiff.length >= 1) {
-        return {
-          error: true,
-          errorPayload: {
-            code: MISMATCH_PARAMETERS,
-            data: parametersDiff,
-            message: 'Parameters missing.'
+      if (
+        typeof requiredAttributes === 'object' &&
+        Object.keys(requiredAttributes).length
+      ) {
+        const payload: { [key: string]: any } = {
+          error: false,
+          code: UNDEFINED_OR_NULL_ATTRIBUTES,
+          errorPayload: {}
+        };
+  
+        Object.keys(requiredAttributes).forEach((attribute: any) => {
+          const accessor = requiredAttributes[attribute];
+          let val;
+          if (typeof accessor === 'function') {
+            val = accessor(target[attribute]);
+          } else {
+            val = target[attribute];
           }
-        };
-      }
-
-      const payload: { [key: string]: any } = {
-        error: false,
-        code: UNDEFINED_OR_NULL_ATTRIBUTES,
-        errorPayload: {}
-      };
-
-      Object.keys(requiredAttributes).forEach((attribute: any) => {
-        const accessor = requiredAttributes[attribute];
-        let val;
-        if (typeof accessor === 'function') {
-          val = accessor(target[attribute]);
-        } else {
-          val = target[attribute];
+          const validator = validationObject[attribute];
+          const result = validator(val).validate();
+  
+          if (result.error) {
+            payload.error = true;
+            payload.errorPayload[attribute] = result;
+          }
+        });
+        if (payload.error) {
+          return {
+            ...payload
+          };
         }
-        const validator = validationObject[attribute];
-        const result = validator(val).validate();
-
-        if (result.error) {
-          payload.error = true;
-          payload.errorPayload[attribute] = result;
-        }
-      });
-      if (payload.error) {
-        return {
-          ...payload
-        };
       }
+      
       return {
         error: false,
         result: func.bind(target)(argumentObject)
@@ -219,13 +281,47 @@ function requiredFunctionParameters(
   };
 }
 
+/**
+ * Wrapper Function around the client/user class
+ * # Example
+ *  class Area {
+ *    constructor(arg) {
+ *      this.length = arg.length;
+ *      this.breadth = arg.breadth; 
+ *    }
+ *    
+ *    calculateArea() {
+ *      return this.length * this.breadth;
+ *    }
+ *  } 
+ * 
+ *  import { wrapWithType }, Types from 'type';
+ * 
+ *  const typeProtocol = {
+ *    length: Types.NumberType({
+ *      range: [1, 100]
+ *    }),
+ *    breadth: Types.NumberType(),
+ *    calculateArea: Types.FunctionType({
+ *      requiredAttributes: {
+ *        length: (k: any) => k,
+ *        breadth: (k: any) => k
+ *      }
+ *    })
+ *  }
+ * 
+ *  Area = wrapWithType(validtionObject)(Area);
+ *  #
+ * @param {object} validationObject 
+ * @returns {Function}
+ */
 export function wrapWithType(validationObject: any) {
   return function(classObject: any) {
-    // make the new prototype
+    // make the new prototype by wrapping around user defined class methods
     const methodsOnClass = Object.getOwnPropertyNames(classObject.prototype);
     const propsOnValidationObject = Object.keys(validationObject);
     methodsOnClass.forEach(method => {
-      // if the method exists on validation Object
+      // if the method exists on validation Object, then wrap around the prototype method
       if (propsOnValidationObject.indexOf(method) !== -1) {
         const requiredList = validationObject[method].requiredParams;
         const requiredAttributes = validationObject[method].requiredAttributes;
@@ -238,12 +334,18 @@ export function wrapWithType(validationObject: any) {
     });
 
     return function(args: any) {
-      // we have a validation object and the actual argument object passed to the object construction
-      const v = validateObjectConstruction(args, validationObject);
-      if (v.error) {
-        throw v;
-      }
+      if (
+        (validationObject.__constructorParameters &&
+        Array.isArray(validationObject.__constructorParameters)) ||
+        Object.keys(args).length
+      ) {
+          const v = validateObjectConstruction(args, validationObject);
+          if (v.error) {
+            throw v;
+          }
+        }
       const target = new classObject(args);
+
       return target;
     };
   };
